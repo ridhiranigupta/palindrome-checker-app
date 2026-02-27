44e0d9e1e9d3a91de6afd3f09f4e19e84f32ed90
ridhiranigupta <ridhigupta98452@gmail.com>
2026-02-27 14:40:21 +0530

    welcome message

---
+++ app/src/PalindroneCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindroneCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("welcome to the palindrome");
+        System.out.println("version:1.0");
+        System.out.println("system initialized successfully");
+    }
+}
+
+
+
+
+
